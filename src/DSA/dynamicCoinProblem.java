07807756abd3aae07ab5cad6855eafb9d27c6b2a
Yashsvigagania <gaganiayashsvi@gmail.com>
2026-03-29 21:29:38 +0530

    init commit

--- src/DSA/dynamicCoinProblem.java
+++ src/DSA/dynamicCoinProblem.java
@@ -1,6 +1,6 @@
 package DSA;
 import java.util.*;
-public class coin {
+public class dynamicCoinProblem {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int sum=sc.nextInt();
