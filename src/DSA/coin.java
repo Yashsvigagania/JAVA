e038d447c81d389b7864f68b54545b5dcbe873fa
Yashsvigagania <gaganiayashsvi@gmail.com>
2026-03-29 21:28:24 +0530

    init commit

---
+++ src/DSA/coin.java
@@ -0,0 +1,21 @@
+package DSA;
+import java.util.*;
+public class coin {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int sum=sc.nextInt();
+        int n=sc.nextInt();
+        int[] coins=new int[n];
+        for(int i=0;i<n;i++){
+            coins[i]=sc.nextInt();
+        }
+        int[] dp=new int[sum+1];
+        dp[0]=1;
+        for(int coin:coins){
+            for(int i=coin;i<=sum;i++){
+                dp[i]+=dp[i-coin];
+            }
+        }
+        System.out.println(dp[sum]);
+    }
+}
