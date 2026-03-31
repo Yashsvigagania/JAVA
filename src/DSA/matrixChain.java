8e5dfb86c471f6e9975b7e0956d4d2bc6c7f71ab
Yashsvigagania <gaganiayashsvi@gmail.com>
2026-03-31 22:37:51 +0530

    init commit

---
+++ src/DSA/matrixChain.java
@@ -0,0 +1,31 @@
+package DSA;
+import java.util.*;
+public class matrixChain {
+    static int matrixChainMultiplication(int[] arr,int n){
+        int[][] dp=new int[n][n];
+        for(int l=2;l<n;l++){
+            for(int i=1;i<n-l+1;i++){
+                int j=i+l-1;
+                dp[i][j]=Integer.MAX_VALUE;
+                for(int k=i;k<j;k++){
+                    int cost=dp[i][k]+dp[k+1][j]+arr[i-1]*arr[k]*arr[j];
+                    if(cost<dp[i][j]){
+                        dp[i][j]=cost;
+                    }
+                }
+            }
+        }
+        return dp[1][n-1];
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int result=matrixChainMultiplication(arr,n);
+        System.out.println(result);
+        sc.close();
+    }
+}
