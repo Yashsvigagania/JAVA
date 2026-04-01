df35133f1a07cddc4812a7007a602c256062f1c3
Yashsvigagania <gaganiayashsvi@gmail.com>
2026-04-01 22:04:36 +0530

    init commit

--- src/DSA/wordBreak.java
+++ src/DSA/wordBreak.java
@@ -1,6 +1,6 @@
 package DSA;
 import java.util.*;
-public class word {
+public class wordBreak {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
 
