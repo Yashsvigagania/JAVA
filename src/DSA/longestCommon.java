d0f50e5f116168bdd2dab4b1da724e5fdfdff4e9
Yashsvigagania <gaganiayashsvi@gmail.com>
2026-04-03 21:38:00 +0530

    init commit

--- src/DSA/longestCommon.java
+++ src/DSA/longestCommon.java
@@ -13,7 +13,7 @@ public class longestCommon {
         for(int i=0;i<n1;i++){
             for(int j=0;j<n2;j++){
                 for(int k=0;k<n3;k++){
-                    if(s1.charAt(0)==s2.charAt(0) && s1.charAt(0)==s3.charAt(0)){
+                    if(s1.charAt(i-1)==s2.charAt(j-1) && s1.charAt(i-1)==s3.charAt(k-1)){
                         dp[i][j][k]=dp[i-1][j-1][k-1]+1;
                     }
                     else{
