83a5eb631d461c3f1b04b9fadfc82abf4039f32b
Yashsvigagania <gaganiayashsvi@gmail.com>
2026-04-03 21:36:15 +0530

    init commit

---
+++ src/DSA/longestCommon.java
@@ -0,0 +1,28 @@
+package DSA;
+import java.util.*;
+public class longestCommon {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.nextLine();
+        String s2=sc.nextLine();
+        String s3=sc.nextLine();
+        int n1=s1.length();
+        int n2=s2.length();
+        int n3=s3.length();
+        int[][][] dp=new int[n1+1][n2+1][n3+1];
+        for(int i=0;i<n1;i++){
+            for(int j=0;j<n2;j++){
+                for(int k=0;k<n3;k++){
+                    if(s1.charAt(0)==s2.charAt(0) && s1.charAt(0)==s3.charAt(0)){
+                        dp[i][j][k]=dp[i-1][j-1][k-1]+1;
+                    }
+                    else{
+                        dp[i][j][k]=Math.max(Math.max(dp[i-1][j][k],dp[i][j-1][k]),dp[i][j][k-1]);
+                    }
+                }
+            }
+        }
+        System.out.println(dp[n1][n2][n3]);
+        sc.close();
+    }
+}
