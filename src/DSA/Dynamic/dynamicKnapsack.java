5e34b4d24b01e254773d9352c284af6a1856492f
Yashsvigagania <gaganiayashsvi@gmail.com>
2026-04-18 18:28:08 +0530

    init commit

--- src/DSA/Dynamic/dynamicKnapsack.java
+++ src/DSA/Dynamic/dynamicKnapsack.java
@@ -6,7 +6,7 @@ public class dynamicKnapsack {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int m=sc.nextInt();
+        int m=sc.nextInt(); //capacity
         int[] value=new int[n];
         int[] weight=new int[n];
         for(int i=0;i<n;i++){
@@ -18,7 +18,7 @@ public class dynamicKnapsack {
         int[][] dp= new int[n+1][m+1];
         for(int i=1;i<=n;i++){
             for(int w=1;w<=m;w++){
-                if(weight[i-1]<=w) {
+                if(weight[i-1]<=w){
                     int take=value[i-1]+dp[i-1][w-weight[i-1]];
                     int skip=dp[i-1][w];
                     dp[i][w]=Math.max(take,skip);
