efe62635aa0d396b5f485da13d3943632db7f9b1
Yashsvigagania <gaganiayashsvi@gmail.com>
2026-04-27 23:41:54 +0530

    init commit

--- src/DSA/Dynamic/FloydWarshall.java
+++ src/DSA/Dynamic/FloydWarshall.java
@@ -1,45 +1,47 @@
 package DSA.Dynamic;
 import java.util.*;
-public class FloydWarshall {
-    static final int INF = Integer.MAX_VALUE / 2;
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int[][] dist = new int[n + 1][n + 1];
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (i == j) {
-                    dist[i][j] = 0;
-                } else {
-                    dist[i][j] = INF;
+public class FloydWarshall{
+    static int INF= (int)1e9;
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        int m= sc.nextInt();
+        int[][] dist=new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==j){
+                    dist[i][j]=0;
+                }
+                else{
+                    dist[i][j]=INF;
                 }
             }
         }
-        for (int i = 0; i < m; i++) {
-            int u = sc.nextInt();
-            int v = sc.nextInt();
-            int w = sc.nextInt();
-            dist[u][v] = w;
+        for(int i=0;i<m;i++){
+            int u=sc.nextInt();
+            int v=sc.nextInt();
+            int w=sc.nextInt();
+            dist[u-1][v-1]=w;
         }
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                for (int k = 1; k <= n; k++) {
-                    if (dist[j][k] < INF && dist[i][k] < INF) {
-                        dist[j][k] = Math.min(dist[j][k], dist[j][i] + dist[i][k]);
+        for(int k=0;k<n;k++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    if(dist[i][k]<INF && dist[k][j]<INF){
+                        dist[i][j]=Math.min(dist[i][j],dist[i][k]+dist[k][j]);
                     }
                 }
             }
         }
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (dist[i][j] == INF) {
-                    System.out.print("INF ");
-                } else {
-                    System.out.print(dist[i][j] + " ");
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(dist[i][j]==INF){
+                    System.out.print("INF"+ " ");
+                }
+                else{
+                    System.out.print(dist[i][j]+" ");
                 }
             }
+            System.out.println();
         }
         sc.close();
     }
