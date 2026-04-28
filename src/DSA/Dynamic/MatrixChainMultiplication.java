046e7d9ddcecd588e04fb83c4f2e0d6a24feaffe
Yashsvigagania <gaganiayashsvi@gmail.com>
2026-04-28 12:13:09 +0530

    init commit

--- src/DSA/Dynamic/MatrixChainMultiplication.java
+++ src/DSA/Dynamic/MatrixChainMultiplication.java
@@ -1,11 +1,17 @@
 package DSA.Dynamic;
 import java.util.*;
 public class MatrixChainMultiplication {
-    static int matrixChainMultiplication(int[] arr,int n){
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
         int[][] dp=new int[n][n];
-        for(int l=2;l<n;l++){
-            for(int i=1;i<n-l+1;i++){
-                int j=i+l-1;
+        for(int len=2;len<n;len++){
+            for(int i=1;i<n-len+1;i++){
+                int j=i+len-1;
                 dp[i][j]=Integer.MAX_VALUE;
                 for(int k=i;k<j;k++){
                     int cost=dp[i][k]+dp[k+1][j]+arr[i-1]*arr[k]*arr[j];
@@ -15,17 +21,6 @@ public class MatrixChainMultiplication {
                 }
             }
         }
-        return dp[1][n-1];
-    }
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int[] arr=new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
-        }
-        int result=matrixChainMultiplication(arr,n);
-        System.out.println(result);
-        sc.close();
+        System.out.println(dp[1][n-1]);
     }
 }
